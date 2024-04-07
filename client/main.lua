d68d61131b39e23e39d938c1579507ed3a10a10d
CptNoHand <90973107+CptNoHand@users.noreply.github.com>
2024-04-07 21:44:06 +0200

    update

--- client/main.lua
+++ client/main.lua
@@ -471,6 +471,10 @@ RegisterNetEvent('qb-garages:client:addHouseGarage', function(house, garageInfo)
     HouseGarages[house] = garageInfo
 end)
 
+RegisterNetEvent('qb-garages:client:removeHouseGarage', function(house)
+    Config.HouseGarages[house] = nil
+end)
+
 AddEventHandler('QBCore:Client:OnPlayerLoaded', function()
     CreateBlipsZones()
 end)
