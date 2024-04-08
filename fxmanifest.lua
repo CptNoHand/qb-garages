316699da6ae96d97c046cfac996126f17de8500d
CptNoHand <90973107+CptNoHand@users.noreply.github.com>
2024-04-08 23:53:17 +0200

    Reapply "Merge remote-tracking branch 'upstream/main'"
    This reverts commit f93c74db134d42e487246b094b815e7c4bd14cb4.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,9 @@
 fx_version 'cerulean'
 game 'gta5'
-
-description 'QB-Garages'
-version '1.0.0'
+lua54 'yes'
+author 'Kakarot'
+description 'Allows players to store their vehicles in garages and withdraw job vehicles'
+version '2.0.0'
 
 shared_scripts {
     'config.lua',
@@ -12,9 +13,9 @@ shared_scripts {
 }
 
 client_scripts {
-	'@PolyZone/client.lua',
-    '@PolyZone/BoxZone.lua',
-	'@PolyZone/ComboZone.lua',
+    '@PolyZone/client.lua',
+    '@PolyZone/CircleZone.lua',
+    '@PolyZone/ComboZone.lua',
     'client/main.lua',
 }
 
@@ -23,4 +24,11 @@ server_scripts {
     'server/main.lua'
 }
 
-lua54 'yes'
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/script.js',
+    'html/style.css',
+    'html/logo.png',
+}
